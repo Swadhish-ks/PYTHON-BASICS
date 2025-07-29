4cb712ad3b22f40c5d5cca2f7ec49c7dcd784c3c
swadhish KS <swadhishofficial@gmail.com>
2025-07-29 13:00:51 +0530

    Initial commit of python basics

--- basic2.py
+++ basic2.py
@@ -1,5 +1,11 @@
+
+
 num = int(input("enter a number: "))
-if num % 2 == 0:
-    print(num,"is even")
+
+if num < 100 :
+    if num % 2==0:
+        print(num, "is even")
+    else:
+        print(num, "is odd")
 else:
-    print(num,"is odd")
+    print("number must be less than 100")
